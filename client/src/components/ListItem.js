9358ae40e1129aef5fef78c9f74dcf702b4808ad
Kelly Atmore <kelly.atmore47@gmail.com>
2022-08-08 16:43:05 -0700

    alternate colors for list items fix

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -6,22 +6,20 @@ const ListItem = (props) => {
     <>
       <div className="list w-[300px] h-[240px] rounded-lg m-6 p-4 hover:scale-105 hover:bg-[#6E8DAC] transition ease-in-out">
         <Link to={`/albums/${props.albums.album_id}`}>
-          <div className="">
-            <img
-              className="w-[90px] h-[90px] rounded-lg mb-4"
-              src={props.albums.album_cover}
-            />
-            <p className="album_list_details text-sand">
-              {props.albums.album_name}
-            </p>
-            <p className="album_list_details text-sand">
-              {props.albums.artist_name}
-            </p>
-            <p className="album_list_details text-sand">
-              {props.albums.release_year}
-            </p>
-            <p className="album_list_details text-sand">{props.albums.genre}</p>
-          </div>
+          <img
+            className="w-[90px] h-[90px] rounded-lg mb-4"
+            src={props.albums.album_cover}
+          />
+          <p className="album_list_details text-sand">
+            {props.albums.album_name}
+          </p>
+          <p className="album_list_details text-sand">
+            {props.albums.artist_name}
+          </p>
+          <p className="album_list_details text-sand">
+            {props.albums.release_year}
+          </p>
+          <p className="album_list_details text-sand">{props.albums.genre}</p>
         </Link>
       </div>
     </>
