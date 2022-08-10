3b617fe124ab37a2be226bed9bf722c467a01b81
Kelly Atmore <kelly.atmore47@gmail.com>
2022-08-09 17:12:04 -0700

    add about page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,13 +10,16 @@ const Home = () => {
         </h1>
         <h1 className="text-[150px] text-sand pl-96 tracking-wide">ALBUMS</h1>
         <p className="subline text-sand tracking-wider">
-          A curated list of the 50 albums that changed music
+          A curated list of 20 albums that changed music
         </p>
         <div>
           <button className="explore pt-24 text-rust tracking-widest flex items-center">
-            <Link to="albums">Explore the Albums</Link>
+            <Link to="/albums">Explore the Albums</Link>
 
-            <img src="/icons/right-arrow.png" className="w-8 pl-2" />
+            <img
+              src="/icons/right-arrow.png"
+              className="w-8 pl-2 hover:scale-110 transition ease-in ease-out"
+            />
           </button>
         </div>
       </div>
