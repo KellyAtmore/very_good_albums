130841345f3262fda650fa14419d54ed4f35c72e
Kelly Atmore <kelly.atmore47@gmail.com>
2022-08-20 18:21:24 -0700

    update colors on list items

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -4,24 +4,22 @@ import { Link } from "react-router-dom";
 const ListItem = (props) => {
   return (
     <>
-      <div className="list w-[300px] h-[240px] rounded-lg m-6 p-4 hover:scale-105 hover:bg-[#6E8DAC] transition ease-in-out">
+      <div className="list w-[300px] h-[240px] rounded-lg m-6 p-4 hover:scale-105 hover:bg-[#dc5c44] transition ease-in-out">
         <Link to={`/albums/${props.albums.album_id}`}>
           <img
             className="w-[90px] h-[90px] rounded-lg mb-4"
             src={props.albums.album_cover}
           />
-          <p className="album_list_details text-charcoal">
+          <p className="album_list_details text-sand">
             {props.albums.album_name}
           </p>
-          <p className="album_list_details text-charcoal ">
+          <p className="album_list_details text-sand ">
             {props.albums.artist_name}
           </p>
-          <p className="album_list_details text-charcoal">
+          <p className="album_list_details text-sand">
             {props.albums.release_year}
           </p>
-          <p className="album_list_details text-charcoal">
-            {props.albums.genre}
-          </p>
+          <p className="album_list_details text-sand">{props.albums.genre}</p>
         </Link>
       </div>
     </>
